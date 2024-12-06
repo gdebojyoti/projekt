414456cf68c2db11bb325a369a7a1fcf4e41b82d
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-12-06 17:37:32 +0530

    A bit of CSS.

--- components/Table.tsx
+++ components/Table.tsx
@@ -2,20 +2,26 @@ import Book from "@/types/book"
 
 const BookTable = ({ data }: { data: Book[] }) => {
   return (
-    <table>
+    <table className="border-spacing-y-2 border-separate">
       <thead>
         <tr>
-          <td>Rating</td>
+          <td className="w-10 text-center">#</td>
+          <td className="w-20 text-center">Rating</td>
           <td>Title</td>
         </tr>
       </thead>
 
       <tbody>
-        {data.map(({ key, title, rating }) => (
+        {data.map(({ key, title, thumbnailUrl, rating }, index) => (
           <tr key={key}>
-            <td>{rating}</td>
-            <td>
-              {title}
+            <td className="text-center">{index + 1}</td>
+            <td className="text-center">{rating}</td>
+            <td className="flex items-center">
+              <img
+                className="w-[50px] h-[75px] object-cover"
+                src={thumbnailUrl}
+              />
+              <span className="ml-4">{title}</span>
             </td>
           </tr>
         ))}
