c060e9089c0124aae68a39c7a3081ad99730bb54
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-12-31 05:46:23 +0530

    UI updates.

--- components/Table.tsx
+++ components/Table.tsx
@@ -5,28 +5,19 @@ import Book from "@/types/book"
 const BookTable = ({ data }: { data: Book[] }) => {
   return (
     <table className="border-spacing-y-2 border-separate">
-      <thead>
-        <tr>
-          <td className="w-16 text-center">Serial #</td>
-          <td className="w-20 text-center">Rating</td>
-          <td className="w-20 text-center">Count</td>
-          <td>Title</td>
-        </tr>
-      </thead>
-
       <tbody>
-        {data.map(({ key, title, rating, totalRatings, url }, index) => (
+        {data.map(({ key, title, rating, totalRatings, url, publisher }, index) => (
           <tr key={key}>
-            <td className="text-center">{index + 1}</td>
-            <td className="text-center">{rating}</td>
-            <td className="text-center">{totalRatings}</td>
-            <td className="flex items-center">
-              {/* <img
-                className="w-[50px] h-[75px] object-cover"
-                src={thumbnailUrl}
-              /> */}
-              <Image src='/images/thumbnail.jpg' alt='fallback logo' width={50} height={75} />
-              <a className="ml-4" href={url}>{title}</a>
+            <td className="text-center"># {index + 1}</td>
+            <td>
+              <div className='flex px-4 items-start' key={key}>
+                <Image src='/images/thumbnail.jpg' alt='fallback logo' width={50} height={71} className='rounded-md shrink-0' />
+                <div className='ml-2 flex-grow min-w-0'>
+                  <div className='text-sm leading-normal truncate'>{title}</div>
+                  <div className='text-xs mt-1 opacity-60 leading-normal'>⭐ {rating} | {totalRatings} votes</div>
+                  <div className='mt-1 px-2 py-1 text-xs rounded-full bg-gray-400 inline-block'>{publisher}</div>
+                </div>
+              </div>
             </td>
           </tr>
         ))}
